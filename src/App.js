f4ae3fb7b7de47053a8eeb477b783d66b299137d
n1kko777 <3734489@gmail.com>
2021-04-01 22:04:55 +0500

    Fix body scroll

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Portfolio } from "./pages/Portfolio";
 import { Contacts } from "./pages/Contacts";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contacts" component={Contacts} />
-      </Switch>
-    </BrowserRouter>
+    <SimpleBar style={{ maxHeight: "100vh" }}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contacts" component={Contacts} />
+        </Switch>
+      </BrowserRouter>
+    </SimpleBar>
   );
 }
 
